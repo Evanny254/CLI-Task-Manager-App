4c0f139138d7d3335e10330438968d8329a77f9a
Evanny254 <evancherry254@gmail.com>
2023-12-12 19:08:28 +0300

    update lib/database.py

---
+++ lib/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+
+engine = create_engine('sqlite:///lib.db')
+
+# Create tables
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
